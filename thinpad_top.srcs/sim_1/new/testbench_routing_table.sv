b187b2f557874df910d2c5452119e8efd5b6eb35
LyricZhao <lyricz@yeah.net>
2019-10-07 21:37:56 +0800

    Add test cases

--- thinpad_top.srcs/sim_1/new/testbench_routing_table.sv
+++ thinpad_top.srcs/sim_1/new/testbench_routing_table.sv
@@ -20,6 +20,59 @@ wire lookup_output_valid;
 wire [`IPV4_WIDTH-1:0] lookup_nexthop;
 
 initial begin
+    clk = 0;
+    rst = 1;
+    lookup_valid = 0;
+    insert_valid = 0;
+    lookup_insert_addr = 0;
+    insert_nexthop = 0;
+    insert_mask_len = 0;
+    #100
+    rst = 0;
+
+    /* Insert 10.0.0.1/16, n=1.1.1.1 */
+    repeat (2) @ (posedge clk);
+    insert_valid <= 1;
+    lookup_insert_addr <= 32'h0x0a000001;
+    insert_nexthop <= 32'h0x01010101;
+    insert_mask_len <= 5'h0x10;
+    repeat (1) @ (posedge clk);
+    insert_valid <= 0;
+    repeat (70) @ (posedge clk);
+
+    /* Insert 10.0.0.1/32, n=2.2.2.2 */
+    // repeat (2) @ (posedge clk);
+    // insert_valid <= 1;
+    // lookup_insert_addr <= 32'h0x0a000001;
+    // insert_nexthop <= 32'h0x02020202;
+    // insert_mask_len <= 5'h0x20;
+    // repeat (1) @ (posedge clk);
+    // insert_valid <= 0;
+    // repeat (70) @ (posedge clk);
+
+    /* Lookup 10.0.1.1 */
+    repeat (2) @ (posedge clk);
+    lookup_valid <= 1;
+    lookup_insert_addr <= 32'h0x0a000101;
+    repeat (1) @ (posedge clk);
+    lookup_valid <= 0;
+    repeat (40) @ (posedge clk);
+
+    /* Lookup 17.2.1.1 */
+    repeat (2) @ (posedge clk);
+    lookup_valid <= 1;
+    lookup_insert_addr <= 32'h0x11020101;
+    repeat (1) @ (posedge clk);
+    lookup_valid <= 0;
+    repeat (40) @ (posedge clk);
+
+    /* Lookup 10.0.0.1 */
+    repeat (2) @ (posedge clk);
+    lookup_valid <= 1;
+    lookup_insert_addr <= 32'h0x0a000001;
+    repeat (1) @ (posedge clk);
+    lookup_valid <= 0;
+    repeat (40) @ (posedge clk);
 end
 
 always clk = #10 ~clk;
