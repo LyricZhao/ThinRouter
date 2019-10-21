df0141974a214e68647cdb3d4672cb4bd159cdeb
LyricZhao <lyricz@yeah.net>
2019-10-21 18:53:26 +0800

    修正了Trie结构逻辑 #2

--- thinpad_top.srcs/sources_1/new/routing_table.sv
+++ thinpad_top.srcs/sources_1/new/routing_table.sv
@@ -37,15 +37,15 @@ logic [`BLCK_ENTRY_WIDTH-1:0] write_data, entry_data;
 /* Assign */
 wire [`BITS_PER_STEP-1:0] current_bits;
 wire [`BLCK_INDEX_WIDTH-1:0] entry_index;
-wire [`BLCK_COVER_WIDTH-1:0] entry_cover;
-wire [`IPV4_WIDTH-1:0] entry_index_nexthop;
+wire [`IPV4_WIDTH-1:0] entry_nexthop;
+wire [`NODE_INDEX_WIDTH-1:0] entry_next_index;
 wire entry_state;
 
 assign current_bits = addr_saved[`IPV4_WIDTH-1:`IPV4_WIDTH-`BITS_PER_STEP];
 assign entry_index = index * `NODE_ENTRY_WIDTH + `BLCK_ENTRY_WIDTH * current_bits;
 assign entry_cover = entry_data[`BLCK_ENTRY_WIDTH-1:`BLCK_ENTRY_WIDTH-`BLCK_COVER_WIDTH];
-assign entry_index_nexthop = entry_data[`IPV4_WIDTH-1:0];
-assign entry_state = entry_data[`IPV4_WIDTH];
+assign entry_nexthop = entry_data[`IPV4_WIDTH+`NODE_INDEX_WIDTH-1:`NODE_INDEX_WIDTH]
+assign entry_next_index = entry_data[`NODE_INDEX_WIDTH-1:0];
 
 /* XPM Ram */
 xpm_memory_spram #(
@@ -86,7 +86,6 @@ always_ff @(posedge clk) begin
         case (state)
             READY: begin
                 if (insert_valid) begin
-                    /* New insertion, TODO */
                     lookup_insert_ready <= 0;
                     addr_saved <= lookup_insert_addr;
                     nexthop_saved <= insert_nexthop;
@@ -94,10 +93,9 @@ always_ff @(posedge clk) begin
                     bits_left <= `BITS_PER_STEP - (insert_mask_len & (`BITS_PER_STEP - 1));
                     state <= INSERT;
                 end else if (lookup_valid) begin
-                    /* New lookup */
-                    addr_saved <= lookup_insert_addr;
                     lookup_insert_ready <= 0;
                     lookup_output_nexthop <= 0;
+                    addr_saved <= lookup_insert_addr;
                     state <= LOOKUP;
                 end
                 index <= 0;
@@ -107,8 +105,8 @@ always_ff @(posedge clk) begin
             end
 
             LOOKUP: begin
-                if (entry_state) begin
-                    lookup_output_nexthop <= entry_index_nexthop;
+                lookup_output_nexthop <= entry_nexthop;
+                if (!entry_next_index) begin    
                     state <= READY;
                     lookup_output_valid <= 1;
                     lookup_insert_ready <= 1;
@@ -119,10 +117,15 @@ always_ff @(posedge clk) begin
             end
 
             INSERT: begin
+                if (shift == 0) begin
 
+                end else begin
+
+                end
             end
 
             default: begin
+                /* Nothing */
             end
         endcase
     end
