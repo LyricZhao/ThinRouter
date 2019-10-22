7462401b15d7eea8cd171bd705ac8dd6b5993017
涂轶翔 <tyx17@mails.tsinghua.edu.cn>
2019-10-22 13:04:54 +0800

    可以正确解析样例的 IP 和 ARP 数据包并打印到 conole #10

--- thinpad_top.srcs/sources_1/manager/io_manager.sv
+++ thinpad_top.srcs/sources_1/manager/io_manager.sv
@@ -4,6 +4,8 @@
 针对数据包是 IP 还是 ARP 会分别交给 ip_packet_manager 和 arp_packet_manager 来处理
 */
 
+`include "debug.vh"
+
 module io_manager (
     // 由父模块提供各种时钟
     input   wire    clk_io,             // IO 时钟
@@ -25,6 +27,16 @@ module io_manager (
 
 typedef byte unsigned u8;
 
+// 初始化用，先用 initial 驱动 (todo)
+reg reset;
+initial begin
+    reset = 0;
+    #10;
+    reset = 1;
+    #10;
+    reset = 0;
+end
+
 /****************************
  * 数据包内容，不包括 preamble
  ***************************/
@@ -38,7 +50,9 @@ bit [47:0]  src_mac;        // 来源 MAC 地址
 // VlanId        2b
 bit [1:0]   vlan_id;        // VLAN 接口编号
 // Protocol     16b
-// - protocol:          16'     IP or ARP
+enum {
+    IP, ARP
+} protocol;                 // 协议
 
 /******************************************************************
  * ARP 包
@@ -80,12 +94,7 @@ bit [15:0]  ip_total_size;  // IPv4 总长度（包括 IPv4 头和数据）
 // IpTtl         8b
 bit [7:0]   ip_ttl;         // 剩余转发次数
 // IpProtocol    8b
-enum bit [7:0] {
-    // (ref: https://en.wikipedia.org/wiki/List_of_IP_protocol_numbers)
-    ICMP = 1,
-    TCP  = 6,
-    UDP  = 17
-} ip_protocol;              // IP 协议
+bit [7:0]   ip_protocol;    // IP 协议
 // IpJunk4      16b
 // - header checksum:   16'     TODO (maybe)
 // IpSrcIp      32b
@@ -103,15 +112,16 @@ enum {
     DstMac,         // 48b
     SrcMac,         // 48b
     VlanJunk,       // 30b
-    VlanId,         //  2b
+    // VlanId,      //  2b
     Protocol,       // 16b
     ArpJunk,        // 64b
     ArpSenderMac,   // 48b
     ArpSenderIp,    // 32b
     ArpTargetMac,   // 48b
     ArpTargetIp,    // 32b
+    ArpComplete,    // 等待 last
     IpJunk1,        //  4b
-    IpHeaderSize,   //  4b
+    // IpHeadSize,  //  4b
     IpJunk2,        //  8b
     IpTotalSize,    // 16b
     IpJunk3,        // 32b
@@ -121,34 +131,309 @@ enum {
     IpSrcIp,        // 32b
     IpDstIp,        // 32b
     IpOptions,      // (ip_header_size - 5) * 4 bytes
-    IpData          // ip_total_size - ip_header_size * 4
+    IpData,         // ip_total_size - ip_header_size * 4
+    IpComplete,     // 等待 last
+    Discard         // 遇到错误，准备丢包
 } decode_next;
 // 下面该写入某个域的第几位
 int read_offset;
-
-// Protocol
-`define PROTOCOL_IPV4   16'h0800;
-`define PROTOCOL_ARP    16'h0806;
-`define PROTOCOL_VLAN   16'h8100;
 
 initial begin
     // 让时间输出格式更好看
     $timeformat(-9, 2, " ns", 20);
 end
 
-// 初始化
-always_ff @ (posedge gtx_resetn) begin
-    rx_ready <= 1;
-    tx_data <= 0;
-    tx_valid <= 0;
-    tx_last <= 0;
-end
+function void handle_arp();
+    $display("Processing ARP packet...");
+    // todo
+endfunction
 
-always_ff @ (posedge clk_io) begin
-    if (rx_valid) begin
+function void handle_ip();
+    $display("Processing IP packet...");
+    // todo
+endfunction
+
+always_ff @ (posedge clk_io or posedge reset) begin
+    // 初始化
+    if (reset) begin
+        $display("init");
+        rx_ready <= 1;
+        tx_data <= 0;
+        tx_valid <= 0;
+        tx_last <= 0;
+        decode_next <= DstMac;
+        read_offset <= 0;
+    end else if (rx_valid) begin
         if (!rx_ready)
             $display("I AM NOT READY!!!");
-        $display("%0t: %x", $realtime, rx_data);
+        //$display("%0t: %x", $realtime, rx_data);
+
+        if (rx_last) begin
+            $display("LAST");
+            case(decode_next)
+                ArpComplete: begin
+                    // todo: CRC checksum
+                    handle_arp();
+                    decode_next <= DstMac;
+                end
+                IpComplete: begin
+                    // todo: CRC checksum
+                    handle_ip();
+                    decode_next <= DstMac;
+                end
+                Discard: begin
+                    $display("packet over and discarded");
+                    decode_next <= DstMac;
+                end
+                default: begin
+                    // 突然收到 last，说明包不完整
+                    $display("?? this packet fails.");
+                end
+            endcase
+        end else begin
+        // 处理输入
+            case(decode_next)
+                DstMac: begin
+                    dst_mac[47 - read_offset -: 8] = rx_data;
+                    if (read_offset == 40) begin
+                        read_offset <= 0;
+                        decode_next <= SrcMac;
+                        `DISPLAY_MAC("Destination MAC:", dst_mac);
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                SrcMac: begin
+                    src_mac[47 - read_offset -: 8] = rx_data;
+                    if (read_offset == 40) begin
+                        read_offset <= 0;
+                        decode_next <= VlanJunk;
+                        `DISPLAY_MAC("Source MAC:", src_mac);
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                VlanJunk: begin
+                    if (read_offset == 24) begin
+                        vlan_id = rx_data[1:0];
+                        read_offset <= 0;
+                        decode_next <= Protocol;
+                        $display("VLAN ID: %x", vlan_id);
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                Protocol: begin
+                    if (read_offset == 0) begin
+                        read_offset <= read_offset + 8;
+                        // IP 和 ARP 的协议前八位都是 0x08，如果不是则丢包
+                        if (rx_data != 8'h08) begin
+                            $display("Invalid Protocol: [7:0]=%x", rx_data);
+                            decode_next <= Discard;
+                        end
+                    end else begin
+                        // 检查后八位
+                        read_offset <= 0;
+                        case (rx_data)
+                            8'h06: begin
+                                decode_next <= ArpJunk;
+                                $display("ARP Packet");
+                            end
+                            8'h00: begin
+                                decode_next <= IpJunk1;
+                                $display("IPv4 Packet");
+                            end
+                            default: begin
+                                decode_next <= Discard;
+                                $display("Invalid Protocol: [15:8]=%x", rx_data);
+                            end
+                        endcase
+                    end
+                end
+                ArpJunk: begin
+                    if (read_offset == 56) begin
+                        read_offset <= 0;
+                        decode_next <= ArpSenderMac;
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                ArpSenderMac: begin
+                    arp_sender_mac[47 - read_offset -: 8] = rx_data;
+                    if (read_offset == 40) begin
+                        read_offset <= 0;
+                        decode_next <= ArpSenderIp;
+                        `DISPLAY_MAC("Sender MAC:", arp_sender_mac);
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                ArpSenderIp: begin
+                    arp_sender_ip[31 - read_offset -: 8] = rx_data;
+                    if (read_offset == 24) begin
+                        read_offset <= 0;
+                        decode_next <= ArpTargetMac;
+                        `DISPLAY_IP("Sender IP:", arp_sender_ip);
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                ArpTargetMac: begin
+                    if (rx_data == 0) begin
+                        if (read_offset == 40) begin
+                            read_offset <= 0;
+                            decode_next <= ArpTargetIp;
+                        end else begin
+                            read_offset <= read_offset + 8;
+                        end
+                    end else begin
+                        $display("Invalid Target MAC with value %x at %0d", rx_data, read_offset);
+                        decode_next <= Discard;
+                    end
+                end
+                ArpTargetIp: begin
+                    arp_target_ip[31 - read_offset -: 8] = rx_data;
+                    if (read_offset == 24) begin
+                        read_offset <= 0;
+                        `DISPLAY_IP("Target IP:", arp_target_ip);
+                        decode_next <= ArpComplete;
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                ArpComplete: begin
+                    //$display("ARP Completed, waiting for rx_last");
+                end
+                IpJunk1: begin
+                    // 确保 IP 协议版本为 4，否则丢包
+                    if (rx_data[7:4] == 4) begin
+                        ip_header_size = rx_data[3:0];
+                        if (rx_data[3:0] < 5) begin
+                            // IP 头大小必须大于等于 5
+                            $display("Invalid IP Header Size: %0dB", rx_data[3:0] * 4);
+                            decode_next <= Discard;
+                        end else begin
+                            $display("IP Header Size: %0dB", ip_header_size * 4);
+                            decode_next <= IpJunk2;
+                        end
+                    end else begin
+                        $display("Invalid IP version: %0d", rx_data[3:0]);
+                        decode_next <= Discard;
+                    end
+                end
+                IpJunk2: begin
+                    // 完全 ignore
+                    decode_next <= IpTotalSize;
+                end
+                IpTotalSize: begin
+                    ip_total_size[15 - read_offset -: 8] = rx_data;
+                    if (read_offset == 8) begin
+                        read_offset <= 0;
+                        decode_next <= IpJunk3;
+                        $display("IP Total Size: %0dB", ip_total_size);
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                IpJunk3: begin
+                    if (read_offset == 24) begin
+                        read_offset <= 0;
+                        decode_next <= IpTtl;
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                IpTtl: begin
+                    ip_ttl = rx_data;
+                    if (rx_data == 0) begin
+                        $display("TTL = 0, Discard");
+                        decode_next <= Discard;
+                    end else begin
+                        $display("IP TTL: %0d", rx_data);
+                        decode_next <= IpProtocol;
+                    end
+                end
+                IpProtocol: begin
+                    ip_protocol = rx_data;
+                    case(rx_data)
+                        1 : $display("IP Protocol: ICMP (1)");
+                        6 : $display("IP Protocol: TCP (6)");
+                        17: $display("IP Protocol: UDP (17)");
+                        default: $display("Unknown IP Protocol: %0d", rx_data);
+                    endcase
+                    decode_next <= IpJunk4;
+                end
+                IpJunk4: begin
+                    if (read_offset == 8) begin
+                        read_offset <= 0;
+                        decode_next <= IpSrcIp;
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                IpSrcIp: begin
+                    ip_src_ip[31 - read_offset -: 8] = rx_data;
+                    if (read_offset == 24) begin
+                        read_offset <= 0;
+                        decode_next <= IpDstIp;
+                        `DISPLAY_IP("Source IP:", ip_src_ip);
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                IpDstIp: begin
+                    ip_dst_ip[31 - read_offset -: 8] = rx_data;
+                    if (read_offset == 24) begin
+                        `DISPLAY_IP("Destination IP:", ip_dst_ip);
+
+                        if (ip_header_size > 5) begin
+                            // 如果 IP 头长度大于默认值，说明存在 Options
+                            decode_next <= IpOptions;
+                            // read_offset 从这个值减到 0
+                            read_offset <= ip_header_size * 4 - 21;
+                        end else if (ip_total_size > 20) begin
+                            // 如果 IP 还有长度放 Data
+                            decode_next <= IpData;
+                            // read_offset 从这个值减到 0
+                            read_offset <= ip_total_size - 21;
+                        end else begin
+                            // 只有一个头，结束了
+                            decode_next <= IpComplete;
+                        end
+                    end else begin
+                        read_offset <= read_offset + 8;
+                    end
+                end
+                IpOptions: begin
+                    if (read_offset != 0)
+                        read_offset <= read_offset - 1;
+                    else begin
+                        if (ip_total_size > ip_header_size * 4) begin
+                            // 还有 Data 部分
+                            decode_next <= IpData;
+                            // read_offset 从这个值减到 0
+                            read_offset <= ip_total_size - ip_header_size * 4 - 1;
+                        end else begin
+                            // 没有了
+                            decode_next <= IpComplete;
+                            read_offset <= 0;
+                        end
+                    end
+                end
+                IpData: begin
+                    if (read_offset != 0) begin
+                        ip_data[read_offset] = rx_data;
+                        read_offset <= read_offset - 1;
+                    end else begin
+                        read_offset <= 0;
+                        decode_next <= IpComplete;
+                    end
+                end
+                IpComplete: begin
+                    //$display("IP Completed, waiting for rx_last");
+                end
+            endcase
+        end
     end
 end
 
