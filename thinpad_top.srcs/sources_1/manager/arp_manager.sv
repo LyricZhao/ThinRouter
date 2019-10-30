8947f2ed3fa998f0b042eff4bb9eff8bdbd4abee
涂轶翔 <tyx17@mails.tsinghua.edu.cn>
2019-10-31 01:51:33 +0800

    对于已经知道的 IP 将不保存 #24

--- thinpad_top.srcs/sources_1/manager/arp_manager.sv
+++ thinpad_top.srcs/sources_1/manager/arp_manager.sv
@@ -2,7 +2,15 @@
 这个模块用来给定 IP 查询 MAC 和 VLAN ID
 
 目前先写的非常简单，只能记 4 个，组合逻辑完成
+
+时序：
+记录
+    同步 clk_internal 时钟，提供 IP MAC VLAN 三个输入，valid 拉高一拍
+查询
+    给 ip_input 输入，组合逻辑输出结果
 */
+
+`include "debug.vh"
 
 module arp_manager (
     input   wire    clk_internal,       // 父模块同步时钟
@@ -21,7 +29,8 @@ module arp_manager (
     input   wire    [47:0]  mac_input,  // 插入的 MAC
     input   wire    [3:0]   vlan_input, // 插入的 VLAN ID
     output  wire    [47:0]  mac_output, // 查询得到的 MAC
-    output  wire    [3:0]   vlan_output // 查询得到的 VLAN ID
+    output  wire    [3:0]   vlan_output,// 查询得到的 VLAN ID
+    output  wire    found               // 表示查询到了结果
 );
 
 bit [1:0]   write_head;
@@ -29,17 +38,11 @@ bit [31:0]  ip_entries[3:0];
 bit [47:0]  mac_entries[3:0];
 bit [3:0]   vlan_entries[3:0];
 
-assign mac_output = 
-    (ip_input == ip_entries[0] ? mac_entries[0] : 48'h0) |
-    (ip_input == ip_entries[1] ? mac_entries[1] : 48'h0) |
-    (ip_input == ip_entries[2] ? mac_entries[2] : 48'h0) |
-    (ip_input == ip_entries[3] ? mac_entries[3] : 48'h0);
-
-assign vlan_output = 
-    (ip_input == ip_entries[0] ? vlan_entries[0] : 3'h0) |
-    (ip_input == ip_entries[1] ? vlan_entries[1] : 3'h0) |
-    (ip_input == ip_entries[2] ? vlan_entries[2] : 3'h0) |
-    (ip_input == ip_entries[3] ? vlan_entries[3] : 3'h0);
+assign {mac_output, vlan_output, found} = 
+    (ip_input == ip_entries[0] ? {mac_entries[0], vlan_output[0], 1b'0} : 52'h0) |
+    (ip_input == ip_entries[1] ? {mac_entries[1], vlan_output[1], 1b'0} : 52'h0) |
+    (ip_input == ip_entries[2] ? {mac_entries[2], vlan_output[2], 1b'0} : 52'h0) |
+    (ip_input == ip_entries[3] ? {mac_entries[3], vlan_output[3], 1b'0} : 52'h0);
 
 always_ff @ (posedge clk_internal) begin
     if (~rst_n) begin
@@ -51,8 +54,16 @@ always_ff @ (posedge clk_internal) begin
             mac_entries[i] <= '0;
             vlan_entries[i] <= '0;
         end
-    end else if (valid) begin
+    end else if (valid && !found) begin
         // 记录当前得到的 IP MAC VLAN
+        // 如果已经存在记录则无视
+        $display("ARP manager saving entry:");
+        $write("\tIP:\t");
+        `DISPLAY_IP(ip_input);
+        $write("\tMAC:\t");
+        `DISPLAY_MAC(mac_input);
+        $display("\tVLAN ID:\t%d", vlan_input);
+        
         ip_entries[write_head] <= ip_input;
         mac_entries[write_head] <= mac_input;
         vlan_entries[write_head] <= vlan_input;
