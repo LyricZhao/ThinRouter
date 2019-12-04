243b934f589ce2e91304ecc23b2f435870cfbdb7
涂轶翔 <tyx17@mails.tsinghua.edu.cn>
2019-12-04 19:58:14 +0800

    计时器连入 thinpad_top #36

--- thinpad_top.srcs/sources_1/thinpad_top.sv
+++ thinpad_top.srcs/sources_1/thinpad_top.sv
@@ -102,6 +102,23 @@ pll clock_gen
     .clk_in1(clk_50M)                 // 外部时钟输入
 );
 
+// 计时器
+// 毫秒（0-999）
+logic [9:0] millisecond;
+// 秒（0-65535）
+logic [15:0] second;
+timer #(
+    // 接入 11M0592 时钟
+    .FREQ(11_059_200),
+    // 计时器最大输出为 65535 秒
+    .OUTPUT_WIDTH(16),
+) timer_inst (
+    .clk(clk_11M0592),
+    .rst_n(locked),
+    .millisecond,
+    .second
+);
+
 // 这里应该是KSZ8795芯片的一些设置，初始化用
 
 eth_conf conf(
