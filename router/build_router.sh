d9caa99b95a379217d0d38acb2f41842c2b1e530
LyricZhao <lyricz@yeah.net>
2019-12-09 23:34:36 +0800

    可以进行cmake/make了 #48

--- router/build_router.sh
+++ router/build_router.sh
@@ -1,7 +1,10 @@
-if [ ! -d "build" ]; then
-  mkdir build
+if [ ! -n "$1" ]; then
+  echo 'Platform(Linux, stdio, macOS or Xilinx) should be given.'
+else
+  if [ ! -d "build" ]; then
+    mkdir build
+  fi
+  cd build
+  cmake .. -DBACKEND=$1
+  make router
 fi
-cd build
-cmake .. -DBACKEND=$1
-make router_hal
-make router
