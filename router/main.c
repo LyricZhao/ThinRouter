e122d33245527f74d5955abaf9999292f46a8681
王征翊 <wangzhen17@mails.tsinghua.edu.cn>
2019-12-10 16:50:22 +0800

    修改一处缩进

--- router/main.c
+++ router/main.c
@@ -31,7 +31,7 @@ in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0100000a, 0x0101000a, 0x0102000a, 0x01030
 
 int main(int argc, char *argv[]) {
     // 0a. 初始化
-    int res = HAL_Init(debug_flag, addrs);
+    int32_t res = HAL_Init(debug_flag, addrs);
     if (res < 0) {
         return res;
     }
@@ -51,115 +51,115 @@ int main(int argc, char *argv[]) {
         update(1, entry);
     }
 
-  uint64_t last_time = 0;
-  while (1) {
-      uint64_t time = HAL_GetTicks();
-      if (time > last_time + 30 * 1000) {
-          // TODO
-          // 把完整的路由表发给每个interface
-          // 参考 RFC2453 3.8
-          // 组播IP: 224.0.0.9, 组播MAC: 01:00:5e:00:00:09
-          printf("30s Timer\n");
-          last_time = time;
-      }
+    uint64_t last_time = 0;
+    while (1) {
+        uint64_t time = HAL_GetTicks();
+        if (time > last_time + 30 * 1000) {
+            // TODO
+            // 把完整的路由表发给每个interface
+            // 参考 RFC2453 3.8
+            // 组播IP: 224.0.0.9, 组播MAC: 01:00:5e:00:00:09
+            printf("30s Timer\n");
+            last_time = time;
+        }
 
-      int mask = (1 << N_IFACE_ON_BOARD) - 1;
-      macaddr_t src_mac, dst_mac;
-      int if_index;
+        int32_t mask = (1 << N_IFACE_ON_BOARD) - 1;
+        macaddr_t src_mac, dst_mac;
+        int32_t if_index;
 
-      res = HAL_ReceiveIPPacket(mask, packet, sizeof(packet), src_mac, dst_mac, 1000, &if_index);
-      if (res == HAL_ERR_EOF) {
-          break;
-      } else if (res < 0) {
-          return res;
-      } else if (res == 0) { // timeout
-          continue;
-      } else if (res > sizeof(packet)) {
-          continue;
-      }
+        res = HAL_ReceiveIPPacket(mask, packet, sizeof(packet), src_mac, dst_mac, 1000, &if_index);
+        if (res == HAL_ERR_EOF) {
+            break;
+        } else if (res < 0) {
+            return res;
+        } else if (res == 0) { // timeout
+            continue;
+        } else if (res > sizeof(packet)) {
+            continue;
+        }
 
-      // 1. 检查checksum
-      if (!validateIPChecksum(packet, res)) {
-          printf("Invalid IP Checksum\n");
-          continue;
-      }
+        // 1. 检查checksum
+        if (!validateIPChecksum(packet, res)) {
+            printf("Invalid IP Checksum\n");
+            continue;
+        }
 
-      in_addr_t src_addr, dst_addr;
+        in_addr_t src_addr, dst_addr;
 
-      // 2. 看目标地址是不是路由器的直连口（是不是路由器本身）
-      uint8_t dst_is_me = 0;
-      for (int i = 0; i < N_IFACE_ON_BOARD; ++ i) {
-          if (memcmp(&dst_addr, &addrs[i], sizeof(in_addr_t)) == 0) {
-              dst_is_me = 1;
-              break;
-          }
-      }
+        // 2. 看目标地址是不是路由器的直连口（是不是路由器本身）
+        uint8_t dst_is_me = 0;
+        for (int i = 0; i < N_IFACE_ON_BOARD; ++ i) {
+            if (memcmp(&dst_addr, &addrs[i], sizeof(in_addr_t)) == 0) {
+                dst_is_me = 1;
+                break;
+            }
+        }
 
-      // TODO
-      // 处理组播地址224.0.0.9
+        // TODO
+        // 处理组播地址224.0.0.9
 
-      if (dst_is_me) {
-          // 3a.1
-          RipPacket rip;
-          if (disassemble(packet, res, &rip)) {
-              if (rip.command == 1) {
-                  // 3a.3 request, 参考 RFC2453 3.9.1
-                  // 只需要回复整个路由表的请求
-                  RipPacket resp;
-                  // TODO: 填完response
-                  // IP
-                  output[0] = 0x45;
-                  // ...
-                  // UDP
-                  // port = 520
-                  output[20] = 0x02;
-                  output[21] = 0x08;
-                  // ...
-                  // RIP
-                  uint32_t rip_len = assemble(&resp, &output[20 + 8]);
-                  // checksum calculation for ip and udp
-                  // if you don't want to calculate udp checksum, set it to zero
-                  // send it back
-                  HAL_SendIPPacket(if_index, output, rip_len + 20 + 8, src_mac);
-              } else {
-                  // 3a.2 response, ref. RFC2453 3.9.2
-                  // 更新路由表
-                  // 更新 metric, if_index, nexthop
-                  // what is missing from RoutingTableEntry?
-                  // TODO: use query and update
-                  // triggered updates? ref. RFC2453 3.10.1
-              }
-          }
-      } else {
-          // 3b.1 dst is not me
-          // forward
-          // beware of endianness
-          uint32_t nexthop, dest_if;
-          if (query(dst_addr, &nexthop, &dest_if)) {
-              // found
-              macaddr_t dest_mac;
-              // direct routing
-              if (nexthop == 0) {
-                  nexthop = dst_addr;
-              }
-              if (HAL_ArpGetMacAddress(dest_if, nexthop, dest_mac) == 0) {
-                  // found
-                  memcpy(output, packet, res);
-                  // update ttl and checksum
-                  forward(output, res);
-                  // TODO: you might want to check ttl=0 case
-                  HAL_SendIPPacket(dest_if, output, res, dest_mac);
-              } else {
-                  // not found
-                  // you can drop it
-                  printf("ARP not found for %x\n", nexthop);
-              }
-          } else {
-              // not found
-              // optionally you can send ICMP Host Unreachable
-              printf("IP not found for %x\n", src_addr);
-          }
-      }
-  }
-  return 0;
+        if (dst_is_me) {
+            // 3a.1
+            RipPacket rip;
+            if (disassemble(packet, res, &rip)) {
+                if (rip.command == 1) {
+                    // 3a.3 request, 参考 RFC2453 3.9.1
+                    // 只需要回复整个路由表的请求
+                    RipPacket resp;
+                    // TODO: 填完response
+                    // IP
+                    output[0] = 0x45;
+                    // ...
+                    // UDP
+                    // port = 520
+                    output[20] = 0x02;
+                    output[21] = 0x08;
+                    // ...
+                    // RIP
+                    uint32_t rip_len = assemble(&resp, &output[20 + 8]);
+                    // checksum calculation for ip and udp
+                    // if you don't want to calculate udp checksum, set it to zero
+                    // send it back
+                    HAL_SendIPPacket(if_index, output, rip_len + 20 + 8, src_mac);
+                } else {
+                    // 3a.2 response, ref. RFC2453 3.9.2
+                    // 更新路由表
+                    // 更新 metric, if_index, nexthop
+                    // what is missing from RoutingTableEntry?
+                    // TODO: use query and update
+                    // triggered updates? ref. RFC2453 3.10.1
+                }
+            }
+        } else {
+            // 3b.1 dst is not me
+            // forward
+            // beware of endianness
+            uint32_t nexthop, dest_if;
+            if (query(dst_addr, &nexthop, &dest_if)) {
+                // found
+                macaddr_t dest_mac;
+                // direct routing
+                if (nexthop == 0) {
+                    nexthop = dst_addr;
+                }
+                if (HAL_ArpGetMacAddress(dest_if, nexthop, dest_mac) == 0) {
+                    // found
+                    memcpy(output, packet, res);
+                    // update ttl and checksum
+                    forward(output, res);
+                    // TODO: you might want to check ttl=0 case
+                    HAL_SendIPPacket(dest_if, output, res, dest_mac);
+                } else {
+                    // not found
+                    // you can drop it
+                    printf("ARP not found for %x\n", nexthop);
+                }
+            } else {
+                // not found
+                // optionally you can send ICMP Host Unreachable
+                printf("IP not found for %x\n", src_addr);
+            }
+        }
+    }
+    return 0;
 }
